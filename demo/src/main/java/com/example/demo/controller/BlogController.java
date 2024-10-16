f4df871a094f39a3d667a224fbfd57b13222e258
pm117pm117 <minjibbc1015@naver.com>
2024-10-16 09:45:15 +0900

    DAY5_구현완료

--- demo/src/main/java/com/example/demo/controller/BlogController.java
+++ demo/src/main/java/com/example/demo/controller/BlogController.java
@@ -25,5 +25,22 @@ public class BlogController {
         return "article_list"; // .HTML 연결
     }
 
-    
+    @GetMapping("/article_edit/{id}") // 게시판 링크 지정
+    public String article_edit(Model model, @PathVariable Long id) {
+        Optional<Article> list = blogService.findById(id); // 선택한 게시판 글
+
+        if (list.isPresent()) {
+            model.addAttribute("article", list.get()); // 존재하면 Article 객체를 모델에 추가
+            } else {
+            // 처리할 로직 추가 (예: 오류 페이지로 리다이렉트, 예외 처리 등)
+                return "error"; // 오류 처리 페이지로 연결
+            }
+        return "article_edit"; // .HTML 연결
+    }
+
+    @PutMapping("/api/article_edit/{id}")
+    public String updateArticle(@PathVariable Long id, @ModelAttribute AddArticleRequest request) {
+        blogService.update(id, request);
+        return "redirect:/article_list"; // 글 수정 이후 .html 연결
+    }
 }
